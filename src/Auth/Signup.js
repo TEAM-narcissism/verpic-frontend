e579688b2e938eb97748afc6c0e2c440035916e9
Tae Hun Kim <zaqxsw1397@gmail.com>
2021-08-20 17:38:01 +0900

    fix: modal이 안뜨는 오류를 고침

--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import Modal from 'styled-react-modal';
+import Modal, { ModalProvider } from 'styled-react-modal';
 import Cookies from 'universal-cookie';
 import styled from "@emotion/styled";
 import tw from "twin.macro";
@@ -48,10 +48,10 @@ const SignupSendButton = styled.button`
 `;
 
 const StyledModal = Modal.styled`
-    font-family: "NanumGothic-Regular";
-    width: 500px;
-    height: 300px;
-    ${tw`bg-gray-100 rounded-lg `}
+font-family: "NanumGothic-Regular";
+width: 500px;
+height: 300px;
+${tw`bg-gray-100 rounded-lg `}
 `;
 
 const ModalButton = styled.div`
@@ -59,7 +59,7 @@ const ModalButton = styled.div`
   ${tw`p-2 border w-1/2 mx-auto mt-20 bg-yellow-400 text-white rounded-lg text-center cursor-pointer`}
 `;
 
-function Signup(props) {
+function Signup() {
 
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
@@ -71,9 +71,9 @@ function Signup(props) {
 
     const modalRef = useRef();
     const [isOpen, setIsOpen] = useState(false);
-    const [modalContent, setModalContent] = useState();
+    const [modalContent, setModalContent] = useState("");
 
-    function toggleModal(e) {
+    function toggleModal() {
         setIsOpen(!isOpen)
     }
 
@@ -123,19 +123,9 @@ function Signup(props) {
     const submitHandler = (e) => {
         e.preventDefault();
 
-        // state에 저장한 값을 가져옵니다.
-        // console.log("firstName= " + firstName);
-        // console.log("lastName= " + lastName);
-        // console.log("birthDate= " + birthDate);
-        // console.log("email= " + email);
-        // console.log("password=" + password);
-        // console.log("Mothertongue= " + Mothertongue);
-        // console.log("StudyLanguage= " + Studylanguage);
-
         if (Mothertongue === Studylanguage) {
             setModalContent("모국어와 공부할 언어는 달라야 해요.")
             setIsOpen(!isOpen);
-
             return;
         }
 
@@ -176,7 +166,7 @@ function Signup(props) {
                                 /[xy]/g,
                                 function (c) {
                                     let r = (Math.random() * 16) | 0,
-                                        v = c == "x" ? r : (r & 0x3) | 0x8;
+                                        v = c === "x" ? r : (r & 0x3) | 0x8;
                                     return v.toString(16);
                                 }
                             );
@@ -192,11 +182,11 @@ function Signup(props) {
                         console.log(err.response);
                         if (err.response) {
                             const statusCode = err.response.data.httpStatus;
-                            if (statusCode == "BAD_REQUEST") {
-                                console.log("잘못된 email 또는 password입니다.");
+                            if (statusCode === "BAD_REQUEST") {
+                                alert("잘못된 email 또는 password입니다.");
                             }
                             else {
-                                console.log("예상치 못한 오류입니다.");
+                                alert("예상치 못한 오류입니다.");
                             }
                         }
                     })
@@ -207,12 +197,10 @@ function Signup(props) {
                     if (statusCode === "BAD_REQUEST") {
                         setModalContent("이미 가입된 회원입니다.")
                         setIsOpen(!isOpen);
-                        window.location.href = '/signup';
                     }
                     else {
                         setModalContent("날짜 형식 오류입니다.")
                         setIsOpen(!isOpen);
-                        window.location.href = '/signup';
                     }
 
                 }
@@ -221,134 +209,134 @@ function Signup(props) {
     }
 
     return (
-        <SignupFormWrapper>
-            <SignupText>회원 가입</SignupText>
+        <ModalProvider>
+            <SignupFormWrapper>
+                <SignupText>회원 가입</SignupText>
 
 
-            <div class="text-gray-600 mb-3 select-none">아래의 항목들을 모두 작성해주세요.</div>
+                <div class="text-gray-600 mb-3 select-none">아래의 항목들을 모두 작성해주세요.</div>
 
-            <SignupQuestionWrapper>
-                <SignupInputWrapper>
-                    <SignupInputText>
-                        <div class="flex">
-                            <span class="mr-3">Step1. 당신의 이름을 입력해주세요.</span>
-                            <span>
-                                {firstName ?
-                                    <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
-                                }
-                            </span>
+                <SignupQuestionWrapper>
+                    <SignupInputWrapper>
+                        <SignupInputText>
+                            <div class="flex">
+                                <span class="mr-3">Step1. 당신의 이름을 입력해주세요.</span>
+                                <span>
+                                    {firstName ?
+                                        <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
+                                    }
+                                </span>
+                            </div>
+                        </SignupInputText>
+                        <SignupInput type="text" name="firstName" value={firstName} onChange={firstNameHandler} required />
+                    </SignupInputWrapper>
+                    <SignupInputWrapper>
+                        <SignupInputText>
+                            <div class="flex">
+                                <span class="mr-3">Step2. 당신의 성을 입력해주세요.</span>
+                                <span>
+                                    {lastName ?
+                                        <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
+                                    }
+                                </span>
+                            </div>
+                        </SignupInputText>
+                        <SignupInput type="text" name="lastName" value={lastName} onChange={lastNameHandler} required />
+                    </SignupInputWrapper>
+                    <SignupInputWrapper>
+                        <SignupInputText>
+                            <div class="flex">
+                                <span class="mr-3">Step3. 당신의 생일을 입력해주세요.</span>
+                                <span>
+                                    {birthDate ?
+                                        <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
+                                    }
+                                </span>
+                            </div>
+                        </SignupInputText>
+                        <SignupInput type="date" name="birthDate" value={birthDate} onChange={birthDateHandler} required />
+                    </SignupInputWrapper>
+                    <SignupInputWrapper>
+                        <SignupInputText>
+                            <div class="flex">
+                                <span class="mr-3">Step4. 당신의 email을 입력해주세요.</span>
+                                <span>
+                                    {email ?
+                                        <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
+                                    }
+                                </span>
+                            </div>
+                        </SignupInputText>
+                        <SignupInput type="email" name="email" value={email} onChange={emailHandler} required />
+                    </SignupInputWrapper>
+                    <SignupInputWrapper>
+                        <SignupInputText>
+                            <div class="flex">
+                                <span class="mr-3">Step5. 당신의 비밀번호를 입력해주세요.</span>
+                                <span>
+                                    {password ?
+                                        <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
+                                    }
+                                </span>
+                            </div>
+                        </SignupInputText>
+                        <SignupInput type="password" name="password" value={password} onChange={passwordHandler} required />
+                    </SignupInputWrapper>
+                    <SignupOptionWrapper>
+                        <SignupOptionText>
+                            <div class="flex">
+                                <span class="mr-3">Step7. 당신의 모국어를 선택해주세요.</span>
+                                <span>
+                                    {Mothertongue ?
+                                        <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
+                                    }
+                                </span>
+                            </div>
+
+                        </SignupOptionText>
+                        <SignupOptionSelect name="mothertongue" value={Mothertongue} onChange={mothertongueHandler}>
+                            <option value="">선택</option>
+                            <option value="KOR">korean</option>
+                            <option value="ENG">english</option>
+                        </SignupOptionSelect>
+                    </SignupOptionWrapper>
+
+                    <SignupOptionWrapper>
+                        <SignupOptionText>
+                            <div class="flex">
+                                <span class="mr-3">Step8. 공부하실 언어를 선택해주세요.</span>
+                                <span>
+                                    {Studylanguage ?
+                                        <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
+                                    }
+                                </span>
+                            </div>
+                        </SignupOptionText>
+                        <SignupOptionSelect name="studylanguage" value={Studylanguage} onChange={studylanguageHandler}>
+                            <option value="">선택</option>
+                            <option value="KOR">korean</option>
+                            <option value="ENG">english</option>
+                        </SignupOptionSelect>
+                    </SignupOptionWrapper>
+                    <StyledModal
+                        isOpen={isOpen}
+                        onBackgroundClick={toggleModal}
+                        onEscapeKeydown={toggleModal}
+                    >
+
+                        <div class="text-center mt-28 text-xl" ref={modalRef}>{modalContent}</div>
+                        <div class="text-align">
+                            <ModalButton onClick={toggleModal}>확인</ModalButton>
                         </div>
-                    </SignupInputText>
-                    <SignupInput type="text" name="firstName" value={firstName} onChange={firstNameHandler} required />
-                </SignupInputWrapper>
-                <SignupInputWrapper>
-                    <SignupInputText>
-                        <div class="flex">
-                            <span class="mr-3">Step2. 당신의 성을 입력해주세요.</span>
-                            <span>
-                                {lastName ?
-                                    <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
-                                }
-                            </span>
-                        </div>
-                    </SignupInputText>
-                    <SignupInput type="text" name="lastName" value={lastName} onChange={lastNameHandler} required />
-                </SignupInputWrapper>
-                <SignupInputWrapper>
-                    <SignupInputText>
-                        <div class="flex">
-                            <span class="mr-3">Step3. 당신의 생일을 입력해주세요.</span>
-                            <span>
-                                {birthDate ?
-                                    <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
-                                }
-                            </span>
-                        </div>
-                    </SignupInputText>
-                    <SignupInput type="date" name="birthDate" value={birthDate} onChange={birthDateHandler} required />
-                </SignupInputWrapper>
-                <SignupInputWrapper>
-                    <SignupInputText>
-                        <div class="flex">
-                            <span class="mr-3">Step4. 당신의 email을 입력해주세요.</span>
-                            <span>
-                                {email ?
-                                    <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
-                                }
-                            </span>
-                        </div>
-                    </SignupInputText>
-                    <SignupInput type="email" name="email" value={email} onChange={emailHandler} required />
-                </SignupInputWrapper>
-                <SignupInputWrapper>
-                    <SignupInputText>
-                        <div class="flex">
-                            <span class="mr-3">Step5. 당신의 비밀번호를 입력해주세요.</span>
-                            <span>
-                                {password ?
-                                    <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
-                                }
-                            </span>
-                        </div>
-                    </SignupInputText>
-                    <SignupInput type="password" name="password" value={password} onChange={passwordHandler} required />
-                </SignupInputWrapper>
-                <SignupOptionWrapper>
-                    <SignupOptionText>
-                        <div class="flex">
-                            <span class="mr-3">Step7. 당신의 모국어를 선택해주세요.</span>
-                            <span>
-                                {Mothertongue ?
-                                    <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
-                                }
-                            </span>
-                        </div>
+                    </StyledModal>
+                    <SignupSendButton disabled={allAnswerFulfiled()} className={allAnswerFulfiled() ? "bg-gray-400 cursor-default" : "bg-yellow-400"} onClick={submitHandler}>
+                        {allAnswerFulfiled() ? "모든 항목을 작성해주세요." : "회원 가입"}
+                    </SignupSendButton>
 
-                    </SignupOptionText>
-                    <SignupOptionSelect name="mothertongue" value={Mothertongue} onChange={mothertongueHandler}>
-                        <option value="">선택</option>
-                        <option value="KOR">korean</option>
-                        <option value="ENG">english</option>
-                    </SignupOptionSelect>
-                </SignupOptionWrapper>
+                </SignupQuestionWrapper>
 
-                <SignupOptionWrapper>
-                    <SignupOptionText>
-                        <div class="flex">
-                            <span class="mr-3">Step8. 공부하실 언어를 선택해주세요.</span>
-                            <span>
-                                {Studylanguage ?
-                                    <FontAwesomeIcon icon={faCheckCircle} class="text-green-500 w-5 mt-1" /> : ""
-                                }
-                            </span>
-                        </div>
-                    </SignupOptionText>
-                    <SignupOptionSelect name="studylanguage" value={Studylanguage} onChange={studylanguageHandler}>
-                        <option value="">선택</option>
-                        <option value="KOR">korean</option>
-                        <option value="ENG">english</option>
-                    </SignupOptionSelect>
-                </SignupOptionWrapper>
-
-                <StyledModal
-                    isOpen={isOpen}
-                    onBackgroundClick={toggleModal}
-                    onEscapeKeydown={toggleModal}
-                >
-
-                    <div class="text-center mt-28 text-xl" ref={modalRef}>{modalContent}</div>
-                    <div class="text-align">
-                        <ModalButton onClick={toggleModal}>확인</ModalButton>
-                    </div>
-                </StyledModal>
-
-
-                <SignupSendButton disabled={allAnswerFulfiled()} className={allAnswerFulfiled() ? "bg-gray-400 cursor-default" : "bg-yellow-400"} onClick={submitHandler}>
-                    {allAnswerFulfiled() ? "모든 항목을 작성해주세요." : "회원 가입"}
-                </SignupSendButton>
-
-            </SignupQuestionWrapper>
-        </SignupFormWrapper >
+            </SignupFormWrapper >
+        </ModalProvider>
     );
 }
 
