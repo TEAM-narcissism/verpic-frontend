02a16edecca3d3692882b8798bbf584d5dccbb45
Tae Hun Kim <zaqxsw1397@gmail.com>
2021-08-21 17:20:13 +0900

    fix: text correction

--- src/locales/en/card.js
+++ src/locales/en/card.js
@@ -2,6 +2,6 @@ export default {
     studystarttimetextprefix: "Study starts at ",
     studystarttimetextsuffix: ":00",
     personcounttagprefix: "A total of ",
-    personcounttagsuffix: " people participate in the study.",
+    personcounttagsuffix: " people will participate in the study.",
     previewbutton: "Go to preview!",
 }
--- src/locales/kr/card.js
+++ src/locales/kr/card.js
@@ -2,6 +2,6 @@ export default {
     studystarttimetextprefix: "스터디는 ",
     studystarttimetextsuffix: "시에 시작합니다.",
     personcounttagprefix: "총 ",
-    personcounttagsuffix: "명이 참여중이에요.",
+    personcounttagsuffix: "명이 스터디를 신청했어요.",
     previewbutton: "예습하시겠어요?",
 }
