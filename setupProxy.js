93aef16b7264d5dd85974fd192588b896873ad95
rlacksgus97 <rlacksgus97@naver.com>
2021-08-27 12:48:43 +0900

    test: test commit for ec2 proxy

--- setupProxy.js
+++ setupProxy.js
@@ -1,15 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        "/front",
-        createProxyMiddleware({
-            target: 'http://localhost:8080/',
-            changeOrigin: true,
-        })
-
-    );
-
-
+  app.use(
+    "/front",
+    createProxyMiddleware({
+      target: "http://spring:8080/",
+      changeOrigin: true,
+    })
+  );
 };
