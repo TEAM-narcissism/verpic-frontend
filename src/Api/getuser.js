67c35037e41b7a64b6c33d914fce9dc4ec62419a
rlacksgus97 <rlacksgus97@naver.com>
2021-08-30 14:15:19 +0900

    url: /api inserted

--- src/Api/getuser.js
+++ src/Api/getuser.js
@@ -1,11 +1,12 @@
+import Cookies from "universal-cookie";
 import axios from "axios";
-import Cookies from 'universal-cookie';
+
 function getuser() {
   return new Promise((resolve, reject) => {
     const cookies = new Cookies();
-    const token = cookies.get('vtoken');
+    const token = cookies.get("vtoken");
     axios
-      .get("http://localhost:3000/users/current-login-user", {
+      .get("/api/users/current-login-user", {
         headers: {
           Authorization: token,
         },
--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,19 +1,15 @@
 import React, { useRef, useState } from "react";
-import { useTranslation } from 'react-i18next';
 
 import AuthButton from "./AuthButton";
 import AuthWrapper from "./AuthWrapper";
 import Cookies from "universal-cookie";
+import GoogleLogin from "react-google-login";
 import InputWithLabel from "./InputWithLabel";
+import Navigator from "../Common/Navigator";
 import axios from "axios";
 import { debounce } from "lodash";
-import Navigator from "../Common/Navigator";
-import GoogleLogin from "react-google-login";
 import generateUuid from "./generateUuid";
-
-
-
-
+import { useTranslation } from "react-i18next";
 
 function Login() {
   const [inputs, setInputs] = useState({
@@ -23,8 +19,7 @@ function Login() {
 
   const emailRef = useRef();
   const passwordRef = useRef();
-  const { t, i18n } = useTranslation('login');
-
+  const { t, i18n } = useTranslation("login");
 
   function storeInfoLogin(accessToken) {
     const cookies = new Cookies();
@@ -39,7 +34,7 @@ function Login() {
     let body = inputs;
     console.log(body);
     await axios
-      .post("/login", body)
+      .post("/api/login", body)
       .then(async (res) => {
         const accessToken = res.data.data.Token;
         storeInfoLogin(accessToken);
@@ -52,32 +47,27 @@ function Login() {
           const statusCode = error.response.data.httpStatus;
           if (statusCode === "BAD_REQUEST") {
             console.log("잘못된 email 또는 password입니다.");
-          }
-          else {
+          } else {
             console.log("예상치 못한 오류입니다.");
           }
         }
       });
   }
 
-
   const googleOauthSuccess = async (res) => {
-
-    console.log(res.tokenId)
+    console.log(res.tokenId);
     const body = {
-      "accessToken": res.tokenId
+      accessToken: res.tokenId,
     };
-    await axios.post("/oauth/google", body).
-      then(async (res) => {
+    await axios
+      .post("/oauth/google", body)
+      .then(async (res) => {
         const accessToken = res.data.data.Token;
         storeInfoLogin(accessToken);
         window.location = "/";
       })
       .catch((err) => console.log(err));
-  }
-
-
-
+  };
 
   const onChange = (e) => {
     const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
@@ -92,14 +82,11 @@ function Login() {
   const allAnswerFulfiled = () => {
     if (inputs.email && inputs.password) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
-  }
-  const nothing = () => {
-
-  }
+  };
+  const nothing = () => {};
 
   const debounceFunc = debounce(onChange, 300);
 
@@ -112,7 +99,6 @@ function Login() {
           name="email"
           placeholder="이메일"
           onChange={debounceFunc}
-
         />
         <InputWithLabel
           label="Password"
@@ -120,16 +106,16 @@ function Login() {
           type="password"
           placeholder="비밀번호"
           onChange={debounceFunc}
-
         />
-        <AuthButton onClick={allAnswerFulfiled() ? postToLogin : nothing}>{t('login')}</AuthButton>
+        <AuthButton onClick={allAnswerFulfiled() ? postToLogin : nothing}>
+          {t("login")}
+        </AuthButton>
 
-        <GoogleLogin className="w-full mt-2 font-semibold border-2 border-black rounded-lg"
+        <GoogleLogin
+          className="w-full mt-2 font-semibold border-2 border-black rounded-lg"
           clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
           onSuccess={googleOauthSuccess}
         ></GoogleLogin>
-
-
       </AuthWrapper>
     </div>
   );
