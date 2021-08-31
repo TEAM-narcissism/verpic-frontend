f7a811596ced098e850d8aa22b50c064466552ad
rlacksgus97 <rlacksgus97@naver.com>
2021-08-31 15:18:20 +0900

    https for sockjs

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import "./App.css";
 
 import * as SockJS from "sockjs-client";
 
+import EditUserInfo from "./User/EditUserInfo";
+import FeedbackPage from "./Feedback/FeedbackPage";
 import Login from "./Auth/Login";
 import Logout from "./Auth/Logout";
 import MainPage from "./Home/MainPage";
 import MatchingPost from "./Api/MatchingPost";
 import Mypage from "./User/Mypage";
 import Preview from "./Preview/Preview";
-import EditUserInfo from "./User/EditUserInfo";
 import PrivateRoute from "./Route/PrivateRoute";
 import PublicRoute from "./Route/PublicRoute";
 import React from "react";
+import ReservationCardList from "./ReservationList/ReservationCardList";
 import Signup from "./Auth/Signup";
 import { Stomp } from "@stomp/stompjs";
 import StudyChat from "./VideoChat/StudyChat";
 import TestPage from "./Home/TestPage";
-import ReservationCardList from "./ReservationList/ReservationCardList";
-import FeedbackPage from "./Feedback/FeedbackPage"
 import VideoCheck from "./VideoChat/VideoCheck";
 
-export const conn = new SockJS("http://localhost:8080/ws-stomp");
+export const conn = new SockJS("https://spring:8080/ws-stomp");
 export const stompconn = Stomp.over(conn);
 
 function App() {
@@ -30,15 +30,19 @@ function App() {
 
       <PrivateRoute component={Logout} path="/logout" exact />
       <PrivateRoute component={StudyChat} path="/studychat/:localRoom" />
-      <PrivateRoute component={ReservationCardList} path="/topic/reservation" exact />
+      <PrivateRoute
+        component={ReservationCardList}
+        path="/topic/reservation"
+        exact
+      />
       <PrivateRoute component={MatchingPost} path="/matching" exact />
       <PrivateRoute component={Mypage} path="/profile/:id" />
       <PrivateRoute component={MainPage} path="/" exact />
       <PrivateRoute component={TestPage} path="/test" exact />
-      <PrivateRoute component={Preview} path="/preview/:previewId"/>
-      <PrivateRoute component={FeedbackPage} path="/feedback/:matchId"/>
+      <PrivateRoute component={Preview} path="/preview/:previewId" />
+      <PrivateRoute component={FeedbackPage} path="/feedback/:matchId" />
       <PrivateRoute component={EditUserInfo} path="/edit/:userId" />
-      <PrivateRoute component={VideoCheck} path="/videochecking/:matchId"/>
+      <PrivateRoute component={VideoCheck} path="/videochecking/:matchId" />
 
       {/* 로그인 / 로그아웃에 관계 없이 접근 가능한 영역 */}
       {/* <Route component={MainPage} path="/" exact /> */}
